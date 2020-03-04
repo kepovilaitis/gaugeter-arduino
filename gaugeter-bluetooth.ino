88dd93cef39b9f38f05124044cf57e1e3865720e
kestutispovilaitis <kiesiusp@gmail.com>
2020-03-04 23:00:57 +0200

    Added final touches

--- gaugeter-bluetooth.ino
+++ gaugeter-bluetooth.ino
@@ -1,48 +1,66 @@
 #include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define LED_PIN 13
+SoftwareSerial _mySerial(7, 8); // RX, TX  
 
-SoftwareSerial mySerial(7, 8); // RX, TX  
-// Connect HM10      Arduino Uno
-//     Pin 1/TXD          Pin 7
-//     Pin 2/RXD          Pin 8
+#define ONE_WIRE_BUS_PIN 2
+
+OneWire _oneWire(ONE_WIRE_BUS_PIN);
+DallasTemperature _sensors(&_oneWire);
+
+float _oilTemperatureValue = 0.00f;
+float _oilPressureValue = 0.00f;
+float _waterTemperatureValue = 0.00f;
+float _chargeValue = 0.00f;
+
+union { float value; byte byteValue[4]; } _oilTemperatureFloatAsBytes;
+union { float value; byte byteValue[4]; } _oilPressureFloatAsBytes;
+union { float value; byte byteValue[4]; } _waterTemperatureFloatAsBytes;
+union { float value; byte byteValue[4]; } _chargeFloatAsBytes;
+union { float value; byte byteValue[4]; } _checksumFloatAsBytes;
+
+double _oilPressurePin = A0;  
+double _chargePin = A1;
 
 void setup() {  
   Serial.begin(9600);
-  // If the baudrate of the HM-10 module has been updated,
-  // you may need to change 9600 by another value
-  // Once you have found the correct baudrate,
-  // you can update it using AT+BAUDx command 
-  // e.g. AT+BAUD0 for 9600 bauds
-  mySerial.begin(9600);
+  _mySerial.begin(9600);
+
+  _sensors.begin();
 }
 
 void loop() {  
-  float randomNr = random(200);
-  float checksum = randomNr * 4;
+  _sensors.requestTemperatures(); 
 
-  union {
-    float number;
-    byte bval[4];
-  } floatAsBytes;
+  _oilTemperatureValue = (float)_sensors.getTempCByIndex(0);
+  _oilPressureValue = analogRead(_oilPressurePin);
+  _waterTemperatureValue = (float)_sensors.getTempCByIndex(1);
+  _chargeValue = analogRead(_chargePin);
 
-  union {
-    float sum;
-    byte bval[4];
-  } floatAsBytesChecksum;
+//  _oilPressureValue = map (_oilPressureValue, 0, 1023, 0, 7);
+  _oilPressureValue = random(70) / 10.0f;
 
-  floatAsBytes.number = randomNr;
-  floatAsBytesChecksum.sum = checksum;
+  Serial.println(_oilPressureValue);
   
-  if (mySerial.available()){
-    mySerial.write("||");
+  _chargeValue = map (_chargeValue, 0, 1023, 0, 145);
+  _chargeValue = _chargeValue / 10;
+  
+  _oilTemperatureFloatAsBytes.value = _oilTemperatureValue;
+  _oilPressureFloatAsBytes.value = _oilPressureValue;
+  _waterTemperatureFloatAsBytes.value = _waterTemperatureValue;
+  _chargeFloatAsBytes.value = _chargeValue;
+  _checksumFloatAsBytes.value = _oilPressureValue + _chargeValue + _waterTemperatureValue + _oilTemperatureValue;
 
-    mySerial.write(floatAsBytes.bval, 4);
-    mySerial.write(floatAsBytes.bval, 4);
-    mySerial.write(floatAsBytes.bval, 4);
-    mySerial.write(floatAsBytes.bval, 4);
+  if (_mySerial.available()){
+    _mySerial.write("||");
 
-    mySerial.write(floatAsBytesChecksum.bval, 4);
+    _mySerial.write(_oilTemperatureFloatAsBytes.byteValue, 4);
+    _mySerial.write(_oilPressureFloatAsBytes.byteValue, 4);
+    _mySerial.write(_waterTemperatureFloatAsBytes.byteValue, 4);
+    _mySerial.write(_chargeFloatAsBytes.byteValue, 4);
+
+    _mySerial.write(_checksumFloatAsBytes.byteValue, 4);
   }
   
   delay(500);
